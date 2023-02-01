827d488b58bd2ae3a1c715094d394bc0900b13c1
Lydia_Numwali <numwalilydia2022@gmail.com>
2023-02-01 14:00:59 +0200

    create profile picture and dashboard

--- db-config.js
+++ db-config.js
@@ -1,15 +1,13 @@
 const mysql=require('mysql');
-const connection=mysql.createConnection({
+const connection =mysql.createConnection({
     host:'localhost',
     user:'root',
     password:'',
     database:'node.js'
 });
-connection.connect(function(error){
-    if(!!error) console.log(error);
-    else {
-        console.log('Database connected')
-    }
-});
+connection.connect((err) => {
+    if (err) console.log("error found," + err);
+    console.log("connected to db successfully");
+  });
 
 module.exports = connection;
--- express.js
+++ express.js
@@ -3,6 +3,8 @@ const express=require('express');
 const ejs=require('ejs');
 const bodyParser=require('body-parser');
 const app=express();
+const mysql = require('mysql');
+
 const multer = require("multer");
 const { diskStorage } = require("multer");
 
@@ -14,66 +16,61 @@ const storage = diskStorage({
     cb(null, `${file.originalname}`);
   },
 });
+
 const upload = multer({ storage });
 
+const connection = mysql.createConnection({
+  host: 'localhost',
+  user: 'root',
+  password: '',
+  database: 'node.js'
+});
+connection.connect((err) => {
+  if (err) console.log("error found," + err);
+  console.log("Database connected");
+});
 app.use(express.json());
 app.use(express.urlencoded({extended: true}));
-app.use(express.static(path.join(__dirname, "")));
+app.use(express.static(path.join(__dirname, "public")));
 
 app.set("view engine", "ejs");
-app.set("views", path.join(__dirname, "views"));
-
 app.get('/', (req,res)=>{
     res.render("signup.ejs");
 })
-let image;
 
-app.post('/register',upload.single("profile_pic"),(req,res,next)=>{
+
+app.post('/register',upload.single('profile_pic'),(req,res,next)=>{
     console.log(req.body);
 
     let first_name = req.body.first_name;
     let last_name = req.body.last_name;
     let email = req.body.email;
     let pwd = req.body.pwd
-    let username = req.body.username
-    image=req.file.filename;
+    let user_name = req.body.username
+    let image=req.file.filename;
 
-    connection.query(`INSERT INTO user_table (first_name,last_name,user_name,email,password,image) VALUES (?,?,?,?,?,?)`,[first_name,last_name,username,email,pwd,image],(err,result)=>{
+connection.query(`INSERT INTO user_table (first_name,last_name,user_name,email,password,image) VALUES (?,?,?,?,?,?)`,[first_name,last_name,user_name,email,pwd,image],(err,result)=>{
         if(err) console.log('error found:',err);
         console.log(result);
-
-    })
-    connection.query(
-        `SELECT image FROM user_table WHERE email=?`,
-        [email],
-        async(err,result)=>{
-            if(err)console.log("err found:",err);
-            image=path.join("images","uploads",result[0].image.toString());
-
-            console.log(image);
-            res.render("dashboard.ejs",{password,username,email,image});        
-  })
+        //res.render("dashboard.ejs",{email,pwd,username,image})
 })
+connection.query(
+  `SELECT image FROM user_table WHERE email = ?`,
+  [email],
+  async (err, result) => {
+    if (err) console.log("err found:", err);
+    console.log(result);
+    // console.log(result[0].image.toString());
 
-app.post("/update", (req, res) => {
-    console.log(req.body);
+    image = path.join("images", "uploads", result[0].image.toString());
+
+    console.log(image);
+    res.render("dashboard.ejs", { pwd, user_name, email, image });
+  }
+);
+  })
   
-    let email = req.body.email;
-    let password = req.body.pwd;
-    let username = req.body.username;
-  
-    connection.query(
-      `UPDATE user_table SET  username = ? , password = ? WHERE email = ? `,
-      [email, username, password],
-      [email],
-      (err, result) => {
-        if (err) console.log("error found:", err);
-        console.log(result);
-  
-        res.render("dashboard.ejs", { email, password, username, image });
-      });
-  });
-  
+ 
 app.listen(3000,()=>{
 console.log('Server running on port 3000');
 })
